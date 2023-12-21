78dbc9fc210be92bf744bb796ae07fdb3d24d7b3
Ekaterina Belova <kbelovaa17@gmail.com>
2023-12-22 00:36:31 +0300

    Commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.use('/', router);
